937474712fcff17b046432f899dcc6946f601535
shoku10 <shoku8544@gmail.com>
2023-08-27 13:55:07 +0900

    コメントした

--- XcodeGit/ViewController.swift
+++ XcodeGit/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // コミットとは保存のこと
         print("初めてのコミット")
     }
 
